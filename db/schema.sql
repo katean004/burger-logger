826ea0629b5763c8be0457c2b0998d7b7e5414a5
kate <kan004@ucr.edu>
2020-08-07 16:28:19 -0700

    set up connection js and schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- Setup database
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+-- Setup tables
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  devour BOOLEAN DEFAULT false,
+  PRIMARY KEY (id)
+);
