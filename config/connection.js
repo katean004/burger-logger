b5636b58bfb79729a95bb4c8e5399d0169b3d606
kate <kan004@ucr.edu>
2020-08-10 22:27:18 -0700

    added connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "GakuenAlice123*",
-  database: "burger_db"
-});
+var connection;
 
 // Make connection.
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "GakuenAlice123*",
+    database: "burger_db"
+  });
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
